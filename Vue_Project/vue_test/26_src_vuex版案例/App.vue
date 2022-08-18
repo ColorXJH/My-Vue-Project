027c43219a568e85a938a1dc6b256c07ae7022eb
Master <1204332316@qq.com>
2022-08-18 18:13:29 +0800

    learn vue2 step by step 109 vuex版案例

---
+++ Vue_Project/vue_test/26_src_vuex版案例/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Count/>
+  </div>
+</template>
+<script>
+import Count from "@/components/Count";
+  export default {
+    name:'App',
+    components:{Count},
+    method:{
+
+    },
+    mounted() {
+      console.log('app',this);
+    }
+
+  }
+</script>
+<style lang="css" scoped>
+
+</style>
