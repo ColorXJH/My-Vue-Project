6e75047766e66967fa4c526f81a38b60bee8fe0f
ColorXJH <1204332316@qq.com>
2022-08-25 21:43:31 +0800

    vue-step-by-step-136 vue2 完结

--- Vue_Project/vue_test/src/main.js
+++ Vue_Project/vue_test/src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue';
 import App from './App';
 
+//引入element ui的组件库
+//import ElementUI from 'element-ui';
+//引入element ui的全部样式
+//import 'element-ui/lib/theme-chalk/index.css';
+
+//按需引入（样式自动分析，自动引入，我们只关注按需引入的组件，前提是按照官网配置）
+import { Button, Row,DatePicker } from 'element-ui';
 Vue.config.productionTip=false;
+
+//应用element ui
+//Vue.use(ElementUI)
+
+//按需注册（注册全局组件）
+Vue.component(Button.name, Button);
+Vue.component(Row.name, Row);
+Vue.component(DatePicker.name, DatePicker);
+
 new Vue({
      el:"#root",
      render: h=>h(App),
